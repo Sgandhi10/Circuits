871a79c06a95ff615702980cacdfa097f92c3fa7
Ayush Rautwar <arautwar@gmail.com>
2020-06-04 01:39:19 -0400

    Add everything

--- visualizer/calculate.js
+++ visualizer/calculate.js
@@ -1,9 +1,17 @@
-var big;
+var big, v;
 big = 0;
+function submit(){
+    big[0][big[1]] = parseFloat(document.getElementById("ANSWER").value)
+    if (verify(big[3], big[0])){
+        var image = document.getElementById("image");
+        image.src = "images/correct.svg";
+    }
+}
+
 function selector() {
     var count, counter, file, nums, rand, varj;
     rand = Number.parseInt(((Math.random() * 10) + 1));
-    file = "circuit"+rand.toString()+".jpg";
+    file = "circuit"+rand.toString()+".svg";
     if ((rand === 1)) {
         nums = {"V": (Number.parseInt((Math.random() * 1000)) / 10), "I": (Number.parseInt((Math.random() * 100)) / 10), "R1": (Number.parseInt((Math.random() * 10000)) / 10), "R2": (Number.parseInt((Math.random() * 10000)) / 10), "R3": (Number.parseInt((Math.random() * 10000)) / 10), "R4": (Number.parseInt((Math.random() * 10000)) / 10)};
         varj = "";
@@ -166,74 +174,85 @@ function selector() {
 }
 
 function dothedo(){
+    var dic;
+    dic = {"R": "&Omega;", "I": "A", "V": "V"};
     big = selector();
-    big[0] = JSON.stringify(big[0])
+    //big[0] = JSON.stringify(big[0])
     var image = document.getElementById("image");
     image.src = "images/"+big[2];
-    document.getElementById("big").innerHTML = big.toString();
+    //document.getElementById("big").innerHTML = big.toString();
+    var s;
+    s = "";
+    for (var key in big[0]){
+        s = s + key+"="+big[0][key].toString()+dic[key.charAt(0)]+"; "
+    }
+
+    s = s.substring(0, s.length-2);
+    document.getElementById("va").innerHTML = s;
+    document.getElementById("vb").innerHTML = big[1].toString();
 }
 
 function verify(rand, nums) {
     var current, resistance, voltage;
-    if ((rand === 1)) {
+    if (rand === 1) {
         current = nums["I"];
         resistance = ((1 / ((1 / nums["R1"]) + (1 / nums["R2"]))) + (1 / ((1 / nums["R4"]) + (1 / nums["R3"]))));
         voltage = nums["V"];
-        return verify(current, resistance, voltage);
+        return error(current, resistance, voltage);
     } else {
-        if ((rand === 2)) {
+        if (rand == 2) {
             current = nums["I"];
             resistance = ((nums["R1"] + nums["R2"]) + (1 / ((1 / nums["R4"]) + (1 / nums["R3"]))));
             voltage = nums["V"];
-            return verify(current, resistance, voltage);
+            return error(current, resistance, voltage);
         } else {
-            if ((rand === 3)) {
+            if (rand == 3) {
                 current = nums["I"];
                 resistance = (nums["R1"] + (1 / ((1 / nums["2"]) + (1 / (nums["R3"] + nums["R4"])))));
                 voltage = nums["V"];
-                return verify(current, resistance, voltage);
+                return error(current, resistance, voltage);
             } else {
-                if ((rand === 4)) {
+                if (rand == 4) {
                     current = nums["I"];
                     resistance = (nums["R1"] + (1 / ((1 / nums["2"]) + (1 / ((nums["R3"] + nums["R4"]) + nums["R5"])))));
                     voltage = nums["V"];
-                    return verify(current, resistance, voltage);
+                    return error(current, resistance, voltage);
                 } else {
-                    if ((rand === 5)) {
+                    if (rand == 5) {
                         current = nums["I"];
                         resistance = (((nums["R1"] + nums["R2"]) + nums["R3"]) + (1 / ((1 / nums["R4"]) + (1 / nums["R5"]))));
                         voltage = nums["V"];
-                        return verify(current, resistance, voltage);
+                        return error(current, resistance, voltage);
                     } else {
-                        if ((rand === 6)) {
+                        if (rand == 6) {
                             current = nums["I"];
                             resistance = (((nums["R1"] + nums["R4"]) + nums["R2"]) + nums["R3"]);
                             voltage = nums["V"];
-                            return verify(current, resistance, voltage);
+                            return error(current, resistance, voltage);
                         } else {
-                            if ((rand === 7)) {
+                            if (rand == 7) {
                                 current = nums["I"];
                                 resistance = ((nums["R1"] + nums["R4"]) + (1 / (((1 / (nums["R2"] + nums["R3"])) + (1 / nums["R5"])) + (1 / nums["R6"]))));
                                 voltage = nums["V"];
-                                return verify(current, resistance, voltage);
+                                return error(current, resistance, voltage);
                             } else {
-                                if ((rand === 8)) {
+                                if (rand == 8) {
                                     current = nums["I"];
                                     resistance = ((nums["R1"] + nums["R6"]) + (1 / (((1 / nums["R2"]) + (1 / nums["R3"])((1 / nums["R5"]))) + (1 / nums["R4"]))));
                                     voltage = nums["V"];
-                                    return verify(current, resistance, voltage);
+                                    return error(current, resistance, voltage);
                                 } else {
-                                    if ((rand === 9)) {
+                                    if (rand == 9) {
                                         current = nums["I"];
                                         resistance = ((nums["R1"] + nums["R2"]) + nums["R3"]);
                                         voltage = nums["V"];
-                                        return verify(current, resistance, voltage);
+                                        return error(current, resistance, voltage);
                                     } else {
-                                        if ((rand === 10)) {
+                                        if (rand == 10) {
                                             current = nums["I"];
                                             resistance = (((1 / ((1 / nums["R1"]) + (1 / nums["R2"]))) + (1 / ((1 / nums["R4"]) + (1 / nums["R5"])))) + nums[3]);
                                             voltage = nums["V"];
-                                            return verify(current, resistance, voltage);
+                                            return error(current, resistance, voltage);
                                         }
                                     }
                                 }
@@ -245,8 +264,9 @@ function verify(rand, nums) {
         }
     }
 }
+
 function error(current, resistance, voltage) {
-    if ((((current + 0.1) > (resistance * voltage)) && ((current - 0.1) < (resistance * voltage)))) {
+    if ((((current + 0.1) > (voltage/resistance)) && ((current - 0.1) < (voltage/resistance)))) {
         return true;
     }
     return false;
