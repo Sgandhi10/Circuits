d7acc513e1a7ab2459fe104483e68f751b0cf477
Soham Gandhi <44321200+Sgandhi10@users.noreply.github.com>
2020-06-08 22:54:06 -0400

    added new circuits

--- calculate.js
+++ calculate.js
@@ -33,10 +33,10 @@ function make_voltage(){
 function selector() {
     var count, counter, file, nums, rand, varj, d;
     var rand_li = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 17, 18];
-    rand = rand_li[Number.parseInt(((Math.random() * rand_li.length)))];
+    rand = rand_li[Number.parseInt((Math.random() * rand_li.length))];
     d = {"V": make_voltage, "I": make_current, "R": make_resistor};
     rand_dict = { 0: ["V", "I", "R1", "R2", "R3", "R4"], 1: ["V", "I", "R1", "R2", "R3", "R4"], 2: ["V", "I", "R1", "R2", "R3", "R4"], 3: ["V", "I", "R1", "R2", "R3", "R4", "R5"], 4: ["V", "I", "R1", "R2", "R3", "R4", "R5"], 5: ["V", "I", "R1", "R2", "R3", "R4"], 6: ["V", "I", "R1", "R2", "R3", "R4", "R5", "R6"], 7: ["V", "I", "R1", "R2", "R3", "R4", "R5", "R6"], 8: ["V", "I", "R1", "R2", "R3"], 9: ["V", "I", "R1", "R2", "R3", "R4"] };
-    v_rand_dicts = {10: ["R5", "R6"], 12: ["R5", "R6"], 14: ["R6", "R7"], 15: ["R5", "R6"], 16: ["R7", "R8"], 17: ["R7", "R8"], 18: ["R4", "R5"]}
+    v_rand_dicts = { 10: ["R5", "R6"], 12: ["R5", "R6"], 14: ["R6", "R7"], 15: ["R5", "R6"], 16: ["R7", "R8"], 17: ["R7", "R8"], 18: ["R4", "R5"] };
     file = "circuit" + rand.toString() + ".svg";
     
     nums = {};
